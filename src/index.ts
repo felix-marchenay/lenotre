b34656eb841cb591a5b6ea91ff64b4ed097f309d
Felix Marchenay <felix.marchenay@opera-energie.com>
2020-08-08 17:25:54 +0200

    caceh

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Authorization } from "./Calendar/Authentification";
 import { Calendrier } from "./Calendar/Calendrier";
 import { OAuth2Client } from "google-auth-library";
+import { Arrosage } from "./Calendar/Arrosage";
+import NodeCache from "node-cache";
 
 (new Authorization).authorize().then((oauth: OAuth2Client) => {
     const calendrier = new Calendrier(oauth);
     
-    calendrier.prochainArrosage();
+    calendrier.prochainArrosage().then((arrosage: Arrosage) => {
+        console.log(arrosage);
+
+        if (arrosage.start < new Date) {
+
+            const cache = new NodeCache();
+
+            const found = cache.get(arrosage.id);
+
+            console.log('aze', found);
+        }
+    });
 });
